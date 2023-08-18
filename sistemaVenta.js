cbb039b5073e6d7ad6d22a140e6a9bafabdd732f
SMCominotti <smcominotti@gmail.com>
2023-08-17 23:18:03 -0300

    practicando clases

--- sistemaVenta.js
+++ sistemaVenta.js
@@ -1,80 +1,95 @@
 class Producto {
-  static contadorProductos = 0;
-
-  cosntructor(nombre, precio) {
-    this._idProducto = ++Producto.contadorProductos;
-    this._nombre = nombre;
-    this._precio = precio;
-  }
-
-  get idProducto() {
-    return this._idProducto;
-  }
-
-  get nombre() {
-    return this._nombre;
-  }
-
-  set nombre(nombre) {
-    this._nombre = nombre;
-  }
-  get precio() {
-    return this._precio;
-  }
-
-  set precio(precio) {
-    this._precio = precio;
-  }
-
-  toString() {
-    return `idProducto: ${this._idProducto}, nombre: $${this._nombre}, precio: ${this._precio}`;
-  } //el primer signo $ es para que indique la moneda.
-}
-
-let producto1 = new Producto("Pantalon", 200);
-let producto2 = new Producto("Camisa", 100);
-
-class Orden {
-  static contadorOrdenes = 0;
-
-  static get MAX_PRODUCTOS() {
-    return 5; //aca indico que el maximo es 5
-  }
-
-  constructor() {
-    this._idOrden = ++Orden.contadorOrdenes;
-    this._idProductos = [];
-    this._contadorProductosAgregados = 0;
-  }
-
-  get _idOrden () {
-    return this._idOrden;
-  }
-
-  agregarProducto(producto){
-    if(this._idProductos.length < Orden.MAX_PRODUCTOS){
-        this._idProductos.push(producto);
-       //tambien podría usar:  this._priductos[this._contadorProductosAgregados++] = producto;
+    static contadorProductos = 0;
+  
+    constructor(nombre, precio) {
+      this._idProducto = ++Producto.contadorProductos;
+      this._nombre = nombre;
+      this._precio = precio;
     }
-    else{
-        console.log('No se pueden agregar más productos');
+  
+    get idProducto() {
+      return this._idProducto;
+    }
+  
+    get nombre() {
+      return this._nombre;
+    }
+  
+    set nombre(nombre) {
+      this._nombre = nombre;
+    }
+  
+    get precio() {
+      return this._precio;
+    }
+  
+    set precio(precio) {
+      this._precio = precio;
+    }
+  
+    toString() {
+      return `idProducto: ${this._idProducto}, nombre: ${this._nombre}, precio: $${this._precio}`;
     }
   }
-
-  calcularTotal(){
-    let totalVenta= 0;
-    for(let producto of this._productos){
-        totalVenta += producto.precio; //totalVenta = totalVenta + producto.precio
+  
+  class Orden {
+    static contadorOrdenes = 0;
+  
+    static get MAX_PRODUCTOS() {
+      return 5;
     }
-    return totalVenta;
+  
+    constructor() {
+      this._idOrden = ++Orden.contadorOrdenes;
+      this._productos = [];
+    }
+  
+    get idOrden() {
+      return this._idOrden;
+    }
+  
+    agregarProducto(producto) {
+      if (this._productos.length < Orden.MAX_PRODUCTOS) {
+        this._productos.push(producto);
+      } else {
+        console.log("No se pueden agregar más productos");
+      }
+    }
+  
+    calcularTotal() {
+      let totalVenta = 0;
+      for (let producto of this._productos) {
+        totalVenta += producto.precio;
+      }
+      return totalVenta;
+    }
+  
+    mostrarOrden() {
+      let productosOrden = "";
+      for (let producto of this._productos) {
+        productosOrden += '\n{' + producto.toString() + " }";
+      }
+  
+      console.log(
+        `Orden: ${this.idOrden} Total: $${this.calcularTotal()}, Productos: ${productosOrden}`
+      );
+    }
   }
-mostrarOrden(){
-    let productosOrden = '';
-    for ( let producto of this._productos){
-        productosOrden += producto.toString() + ' ';
-    }
-
-    console.log (`orden: ${this._idOrden} Total: ${this.calcularTotal()},Productos: ${productosOrden}`)
-}
-
-}
+  
+  let producto1 = new Producto("Pantalon", 200);
+  let producto2 = new Producto("Camisa", 100);
+  
+  let orden1 = new Orden();
+  orden1.agregarProducto(producto1);
+  orden1.agregarProducto(producto2);
+  orden1.mostrarOrden();
+  
+  let orden2 = new Orden();
+  let producto3 = new Producto("cinturon",50);
+  orden2.agregarProducto(producto3);
+  orden2.agregarProducto(producto1);
+  orden2.agregarProducto(producto2);
+  orden2.agregarProducto(producto3);
+  orden2.agregarProducto(producto1);
+  orden2.agregarProducto(producto2);
+  orden2.mostrarOrden(); //al poner 6, solo se agregan los primeros 5 y sale el mensaje que no se pueden agregar mas productos.
